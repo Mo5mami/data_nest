58fc18828ce41f4806bb85ab852cf7848976f32f
mo5mami <mami.mokhtar123@gmail.com>
2020-05-08 18:24:06 +0100

    filter added

--- front_end/src/views/Datasets/Dataset.js
+++ front_end/src/views/Datasets/Dataset.js
@@ -8,6 +8,8 @@ import CardHeader from "components/Card/CardHeader.js";
 import DatasetTable from "components/DatasetTable/DatasetTable"
 
 import axios from "axios"
+import { BottomNavigation, Container } from '@material-ui/core';
+import { BottomNavigationAction } from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
@@ -49,15 +51,19 @@ const useStyles = makeStyles({
 export default function Dataset(props) {
   const classes = useStyles();
   const [datasets, setDatasets] = useState([])
-  
+  const [state, setstate] = useState({datasetType:"all",displayDataset:[]})
 
   
   const getDatasets= ()=>
   { const token = localStorage.getItem('token')
+    //console.log("token : ",token)
     axios.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    let url='http://localhost:5000/api/datasets'
+    
+    
     axios({
       method: 'get',
-      url: 'http://localhost:5000/api/datasets',
+      url: url,
       headers: {
         'Content-Type': 'application/json' 
       },
@@ -65,6 +71,7 @@ export default function Dataset(props) {
       .then( (response)=> {
           if (response.data.success){
             setDatasets(response.data.datasets)
+            setstate({...state,displayDataset:response.data.datasets})
           }
           
       })
@@ -79,22 +86,51 @@ export default function Dataset(props) {
       
     },[]
   )
+  const filterDataset=(type)=>
+  {
+    if(type==="all")
+      setstate({...state,datasetType:type,displayDataset:datasets})
+    if(type==="me")
+    {
+      let user=JSON.parse( localStorage.getItem("user"))
+      
+      setstate({...state,datasetType:type,displayDataset:datasets.filter(element => 
+        {
+         
+          return element.owner===user["_id"]
+        }
+      )})
+    }
+  }
+  const handleType=(e,val)=>
+  {
+   
+    filterDataset(val)
+   
+  }
 
   return (
-   <>
+   <React.Fragment>
    <GridContainer>
       <GridItem xs={12} sm={12} md={12}>
         <Card>
           <CardHeader color="primary">
             <h4 className={classes.cardTitleWhite}>Dataset List</h4>
-            <h2 className={classes.cardCategoryWhite}>
-              Get Your Points !
-            </h2>
+            <BottomNavigation
+              value={state.datasetType}
+              onChange={handleType}
+              showLabels
+              className={classes.root}
+            >
+              <BottomNavigationAction value="all" label="All Datasets"  />
+              <BottomNavigationAction value="me" label="My Datasets"  />
+              
+            </BottomNavigation>
           </CardHeader>
         </Card>
       </GridItem>
 
-      <GridItem xs={12} sm={12} md={12}>
+      {/*<GridItem xs={12} sm={12} md={12}>
         <DatasetTable title="Dataset Mezyena1" description=" this is juste a static example : description of  datasets description of  
         datasets description of  datasets description of  datasets  " points="2"/>
       </GridItem>
@@ -103,16 +139,20 @@ export default function Dataset(props) {
         <DatasetTable title="Dataset Mezyena2" description=" this is juste a static example : description of  datasets description of  
         datasets description of  datasets description of  datasets  " points="2"/>
       </GridItem>
+  
       <h1>Datasets from db </h1>
-      {datasets.map(dataset=>
+      */}
+      {state.displayDataset.map(dataset=>
         <GridItem key={dataset._id} xs={12} sm={12} md={12}>
         <DatasetTable title={dataset.name} description={dataset.description} points={dataset.points} percentage={dataset.percentage}/>
       </GridItem>
       )}
 
       </GridContainer>
-   </>
+      
+      </React.Fragment>
   );
+
 }
 
 
